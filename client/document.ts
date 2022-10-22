068c6f9e32d44bce3ba4fe83b89f246d9ba6f017
vipmath <vipmath0205@gmail.com>
2022-10-23 02:16:38 +0900

    document.ts
    upgrade document.ts

--- client/document.ts
+++ client/document.ts
@@ -103,7 +103,8 @@ export function changePieceCSS(assetUrl: string, family: string, cssFile: string
         case "ordamirror": cssLinkIndex += 19; break;
         case "chak": cssLinkIndex += 20; break;
         case "chennis": cssLinkIndex += 21; break;
-        case "pandemonium": cssLinkIndex += 22; break;		
+        case "pandemonium": cssLinkIndex += 22; break;
+        case "promojanggi": cssLinkIndex += 23; break;		
         default: throw "Unknown piece family " + family;
     }
     const newUrl = `${assetUrl}/piece/${family}/${cssFile}.css`;
